547ccd04e100a0ddadfbd4ea7e59b9500a3c43a3
NikolaDmit <nikoladmit21@gmail.com>
2021-04-05 15:56:40 +0300

    Тест

---
+++ main.js
@@ -0,0 +1,9 @@
+'use strict';
+const Module22 = require('Module22');
+const Module24 = require('Module24');
+const Module25 = require('Module25');
+
+console.log('4. Module21 --->');
+Module22();
+Module24();
+Module25();
