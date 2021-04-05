ef292be70b38e2d46e5b018067e37d99b6be2bc3
workout21 <kolyadm@ukr.net>
2021-04-05 17:56:15 +0300

    workout21 commit

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const Module22 = require('Module22');
 const Module24 = require('Module24');
 const Module25 = require('Module25');
 
-console.log('4. Module21 --->');
+console.log('6. Module21 --->');
+console.log('7. Module21 ---> !!!!!!!!!!!!!!//workout21');
 Module22();
 Module24();
 Module25();
